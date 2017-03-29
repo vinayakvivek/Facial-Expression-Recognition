6b6dd9afaacc132f8ac63b01a156838261bfe73e
sagar1760 <airen_sagar@rediffmail.com>
2017-03-29 23:31:43 +0530

    Add pca.py

---
+++ pca.py
@@ -0,0 +1,35 @@
+from sklearn import svm
+import pandas as pd
+import numpy as nm
+import cv2 as cv
+from sklearn import decomposition as dec
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+fields = nm.genfromtxt('fer2013/fer2013.csv',delimiter=',',dtype=None)
+
+images=[]
+
+for i in range(1,len(fields)):
+    
+    images.append(nm.array(fields[i][1].split(" ")).astype(nm.float))
+
+images=nm.array(images)
+pca=dec.PCA(n_components=3)
+pca.fit(images)
+
+images=pca.transform(images)
+
+
+fig=plt.figure()
+
+ax = fig.add_subplot(111, projection='3d')
+
+
+
+
+clf = svm.SVC(kernel='rbf', gamma=2)
+clf.fit(images[:2000], fields[1:2001,0].astype(nm.float))
+
+print clf.predict(images[2000:2200])- fields[2001:2201,0].astype(nm.float)
+
